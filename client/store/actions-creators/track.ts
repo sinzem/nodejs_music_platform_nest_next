2f269184afcb7637997c9141b8d9af5125ddc1b7
Sergey <sinzem@gmail.com>
2024-08-06 23:46:21 +0300

    searc

--- client/store/actions-creators/track.ts
+++ client/store/actions-creators/track.ts
@@ -13,3 +13,14 @@ export const fetchTracks = () => { /* (функция для получения 
         }
     }
 }
+
+export const searchTracks = (query: string) => { /* (для поиска треков по имени) */
+    return async (dispatch: Dispatch<TrackAction>) => { 
+        try {
+            const response = await axios.get("http://localhost:5000/tracks/search?query=" + query);  /* (добавляем участок пути с именем трека для поиска(придет пропсом)) */
+            dispatch({type: TrackActionTypes.FETCH_TRACKS, payload: response.data});
+        } catch (e) { 
+            dispatch({type: TrackActionTypes.FETCH_TRACKS_ERROR, payload: "Произошла ошибка при загрузек треков"})
+        }
+    }
+}
